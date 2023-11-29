48153ba78ab39dd63b889a55a10e16206c3da7e6
jimmy feriawan <jimyferi@gmail.com>
2023-11-29 08:50:14 +0700

    add : add riverpod state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_application_1/pages/login_screen.dart';
 import 'package:flutter_application_1/route.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+// class MyApp extends StatelessWidget {
+//     const MyApp({super.key});
+
+//     @override
+//     Widget build(BuildContext context) {
+//         // const appTitle = 'Login';
+
+//         return MaterialApp.router(
+//             routerConfig: router,
+//         );
+
+//     }
+// }
+
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends ConsumerWidget {
     const MyApp({super.key});
 
     @override
-    Widget build(BuildContext context) {
+    Widget build(BuildContext context, WidgetRef ref) {
         // const appTitle = 'Login';
+        final appRouter = ref.watch(router);
 
         return MaterialApp.router(
-            routerConfig: router,
+            routerConfig: appRouter,
         );
 
     }
--- lib/route.dart
+++ lib/route.dart
@@ -3,29 +3,34 @@ import 'package:flutter_application_1/pages/edit_profile_screen.dart';
 import 'package:flutter_application_1/pages/login_screen.dart';
 import 'package:flutter_application_1/pages/profile_screen.dart';
 import 'package:flutter_application_1/pages/signup_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-final GoRouter router = GoRouter(
-    initialLocation: '/',
-    
-    routes: <RouteBase>[
-        GoRoute(
-            path: '/',
-            builder: (BuildContext context, GoRouterState state) => const LoginScreen(),   
-        ),
-        GoRoute(
-            path: '/profile',
-            builder: (context, state) => const ProfileScreen(),
-            routes: <RouteBase> [
-                GoRoute(
-                    path: "edit",
-                    builder: (context, state) => const EditProfileScreen(),
-                ),
-            ]
-        ),
-        GoRoute(
-            path: '/signup',
-            builder: (context, state) => const SignupScreen(),
-        ),
-    ]
-);
+final _key = GlobalKey<NavigatorState>();
+
+final router = Provider<GoRouter>((ref) {
+    return GoRouter(
+        navigatorKey: _key,
+        initialLocation: '/',
+        routes: <RouteBase>[
+            GoRoute(
+                path: '/',
+                builder: (BuildContext context, GoRouterState state) => const LoginScreen(),   
+            ),
+            GoRoute(
+                path: '/profile',
+                builder: (context, state) => const ProfileScreen(),
+                routes: <RouteBase> [
+                    GoRoute(
+                        path: "edit",
+                        builder: (context, state) => const EditProfileScreen(),
+                    ),
+                ]
+            ),
+            GoRoute(
+                path: '/signup',
+                builder: (context, state) => const SignupScreen(),
+            ),
+        ]
+    );
+});
