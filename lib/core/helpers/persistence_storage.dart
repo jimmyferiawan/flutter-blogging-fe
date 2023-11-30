149c6fa726071ad43c7b80d596e7cd13a68cffda
jimmy feriawan <jimyferi@gmail.com>
2023-11-30 11:25:02 +0700

    add: shared prefereence helper function

--- lib/core/helpers/persistence_storage.dart
+++ lib/core/helpers/persistence_storage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> setJwtToken(String jwtToken) async{
@@ -13,4 +15,21 @@ Future<String> getJwtToken() async{
 Future<void> removeJwtToken() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove("jwt");   
+}
+
+Future<void> setUsername(String username) async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("username", username);
+}
+
+Future<String> getUsername() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString("username") ?? "";
+}
+
+Future<void> clearSession() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove("username");
+    prefs.remove("jwt");
 }
