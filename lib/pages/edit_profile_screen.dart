621022b9911ad16f43904dd8aae444c1f56534ea
jimmy feriawan <jimyferi@gmail.com>
2023-11-30 14:55:41 +0700

    fix: remove unused code

--- lib/pages/edit_profile_screen.dart
+++ lib/pages/edit_profile_screen.dart
@@ -16,37 +16,21 @@ class EditProfileScreen extends StatelessWidget {
                 leading: GestureDetector(
                     child: const Icon(Icons.close_rounded, color: Colors.white),
                     onTap: () {
-                        // Map<String, dynamic> _userData = GoRouterState.of(context).extra! as Map<String, dynamic>;
-                        // String username = _userData['username']! as String;
-
-                        // getJwtToken().then((value) {
-                            Router.neglect(context, () => context.go("/profile"));
-                        // });
+                        Router.neglect(context, () => context.go("/profile"));
                     }
                 )
             ),
-            // body: const Column(
-            //     crossAxisAlignment: CrossAxisAlignment.stretch,
-            //     children: <Widget>[
-            //         Padding(
-            //             padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-            //             child: _EditProfileForm(),
-            //         ),
-            //     ],
-            // ),
             body: LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints viewportConstraints) {
-                return SingleChildScrollView(
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                    child: ConstrainedBox(
-                        constraints: BoxConstraints(
-                            minHeight: viewportConstraints.maxHeight,
+                    return SingleChildScrollView(
+                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                        child: ConstrainedBox(
+                            constraints: BoxConstraints(
+                                minHeight: viewportConstraints.maxHeight,
+                            ),
+                            child: const _EditProfileForm(),
                         ),
-                        child: const IntrinsicHeight(
-                            child: _EditProfileForm(),
-                        ),
-                    ),
-                );
+                    );
                 },
             ),
         );
@@ -96,16 +80,12 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: TextFormField(
-                            // initialValue: _userData['username']! as String ,
-                            // onChanged: (value) => {onChangeUsername(value)},
                             controller: inputUsernameController,
-                            // focusNode: usernameFieldFocusNode,
                             decoration: const InputDecoration(
                                 floatingLabelBehavior: FloatingLabelBehavior.auto,
                                 labelText: 'Username',
                                 border: OutlineInputBorder()
                             ),
-                            // The validator receives the text that the user has entered.
                             validator: (value) {
                                 if (value == null || value.isEmpty) {
                                     return 'Username tidak boleh kosong';
@@ -117,19 +97,15 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: TextFormField(
-                            // initialValue: _userData['nama']! as String,
-                            // onChanged: (value) => {onChangeUsername(value)},
                             controller: inputNamaController,
-                            // focusNode: usernameFieldFocusNode,
                             decoration: const InputDecoration(
                                 floatingLabelBehavior: FloatingLabelBehavior.auto,
                                 labelText: 'Nama',
                                 border: OutlineInputBorder()
                             ),
-                            // The validator receives the text that the user has entered.
                             validator: (value) {
                                 if (value == null || value.isEmpty) {
-                                    return 'Username tidak boleh kosong';
+                                    return 'Nama tidak boleh kosong';
                                 }
                                     return null;
                             },
@@ -138,24 +114,20 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: TextFormField(
-                            // initialValue: _userData['email']! as String,
-                            // onChanged: (value) => {onChangeUsername(value)},
                             controller: inputEmailController,
-                            // focusNode: usernameFieldFocusNode,
                             decoration: const InputDecoration(
                                 floatingLabelBehavior: FloatingLabelBehavior.auto,
                                 labelText: 'Email',
                                 border: OutlineInputBorder()
                             ),
-                            // The validator receives the text that the user has entered.
                             validator: (value) {
                                 if (value == null || value.isEmpty) {
-                                    return 'This field is required';
+                                    return 'Email tidak boleh kosong';
                                 }
 
                                 // using regular expression
                                 if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                                    return "Please enter a valid email address";
+                                    return "Alamat email tidak valid";
                                 }
 
                                 return null;
@@ -165,16 +137,12 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: TextFormField(
-                            // initialValue: _userData['intro']! as String,
-                            // onChanged: (value) => {onChangeUsername(value)},
                             controller: inpuIntroController,
-                            // focusNode: usernameFieldFocusNode,
                             decoration: const InputDecoration(
                                 floatingLabelBehavior: FloatingLabelBehavior.auto,
                                 labelText: 'Intro',
                                 border: OutlineInputBorder()
                             ),
-                            // The validator receives the text that the user has entered.
                             validator: (value) {
                                 if (value == null || value.isEmpty) {
                                     return 'Username tidak boleh kosong';
@@ -186,10 +154,7 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: TextFormField(
-                            // initialValue: _userData['phone']! as String,
-                            // onChanged: (value) => {onChangeUsername(value)},
                             controller: inpuMobileController,
-                            // focusNode: usernameFieldFocusNode,
                             decoration: const InputDecoration(
                                 prefixText: "(+62) ",
                                 floatingLabelBehavior: FloatingLabelBehavior.auto,
@@ -201,7 +166,6 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                     Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10),
                         child: TextFormField(
-                            // initialValue: _userData['birthdate']! as String,
                             readOnly: true,
                             controller: inpuDateController,
                             onTap: () async{
@@ -215,26 +179,15 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
                                 if(pickedDate != null ){
                                     debugPrint("selected date : ${pickedDate.toString()}");  //get the picked date in the format => 2022-07-04 00:00:00.000
                                     inpuDateController.text = datePickerParser(pickedDate.toString());
-                                    // String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
-                                    // print(formattedDate); //formatted date output using intl package =>  2022-07-04
-                                        //You can format date as per your need
-
-                                    // setState(() {
-                                    //     dateController.text = formattedDate; //set foratted date to TextField value. 
-                                    // });
                                 } else {
                                     debugPrint("Date is not selected");
                                 }
                             },
-                            // onChanged: (value) => {onChangeUsername(value)},
-                            // controller: usernameController,
-                            // focusNode: usernameFieldFocusNode,
                             decoration: const InputDecoration(
                                 floatingLabelBehavior: FloatingLabelBehavior.always,
                                 labelText: 'Tgl. lahir',
                                 border: OutlineInputBorder()
                             ),
-                            // The validator receives the text that the user has entered.
                         ),
                     ),
                     Padding(
@@ -253,6 +206,5 @@ class _EditProfileFormState extends ConsumerState<_EditProfileForm> {
             
             )
         );
-        
     }
 }
