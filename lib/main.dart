03fba36a90c902ce501a75b7e1b8050662641241
jimmy feriawan <jimyferi@gmail.com>
2023-11-30 11:18:36 +0700

    add: redirect ro profile if there is session

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/pages/login_screen.dart';
+import 'package:flutter_application_1/core/helpers/persistence_storage.dart';
 import 'package:flutter_application_1/route.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// void main() => runApp(const MyApp());
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+    String jwt = await getJwtToken();
+    String username = await getUsername();
+    bool isSession = jwt != "" && username != "";
 
-// class MyApp extends StatelessWidget {
-//     const MyApp({super.key});
-
-//     @override
-//     Widget build(BuildContext context) {
-//         // const appTitle = 'Login';
-
-//         return MaterialApp.router(
-//             routerConfig: router,
-//         );
-
-//     }
-// }
-
-void main() => runApp(const ProviderScope(child: MyApp()));
+    runApp(ProviderScope(child: MyApp(isLoggedIn: isSession)));
+}
 
 class MyApp extends ConsumerWidget {
-    const MyApp({super.key});
+    final bool isLoggedIn;
+    const MyApp({super.key, required this.isLoggedIn});
 
     @override
     Widget build(BuildContext context, WidgetRef ref) {
-        // const appTitle = 'Login';
         final appRouter = ref.watch(router);
 
+        if(isLoggedIn) {
+            appRouter.go("/profile");
+        }
+        
         return MaterialApp.router(
             routerConfig: appRouter,
         );
