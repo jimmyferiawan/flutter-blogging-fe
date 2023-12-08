5c22f81adb2500a3d67d8672e90c945c820de9dc
jimmy feriawan <jimyferi@gmail.com>
2023-12-08 09:40:03 +0700

    fix : unhandled error on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/core/helpers/persistence_storage.dart';
 import 'package:flutter_application_1/core/http_services/api_calls.dart';
 import 'package:flutter_application_1/dto/auth_dto.dart';
+import 'package:flutter_application_1/pages/splash_screen.dart';
 import 'package:flutter_application_1/route.dart';
 import 'package:flutter_application_1/store/auth_store.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async{
     WidgetsFlutterBinding.ensureInitialized();
-    String jwt = await getJwtToken();
-    String username = await getUsername();
-    bool isSession = jwt != "" && username != "";
-    UserDataResp userDataResp = !isSession ? UserDataResp.emptyValue() : await getUserData(jwt, username);
-    UserData? userData = userDataResp.error ? null : UserData.fromJson(userDataResp.data);
-
-    runApp(ProviderScope(child: MyApp(isLoggedIn: isSession, userData: userData,)));
+    runApp(const ProviderScope(child: MainApp()));
 }
 
-class MyApp extends ConsumerWidget {
-    final bool isLoggedIn;
-    final UserData? userData;
-    const MyApp({super.key, required this.isLoggedIn, required this.userData});
+class MainApp extends StatefulHookConsumerWidget {
+    const MainApp({super.key});
 
     @override
-    Widget build(BuildContext context, WidgetRef ref) {
+    ConsumerState<ConsumerStatefulWidget> createState() => _MainAppState();
+}
+class _MainAppState extends ConsumerState<MainApp> {
+    late Future<UserData?> _userDataResp;
+
+    @override
+    void initState() {
+        super.initState();
+        _userDataResp = initUserAccount();
+    }
+
+    @override
+    Widget build(BuildContext context) {
         final appRouter = ref.watch(router);
-
-        if(isLoggedIn && (userData != null)) {
-            Future(() {
-               ref.read(userDataStateProvider.notifier).setData(userData); 
-            });
-        }
         
-        return MaterialApp.router(
-            routerConfig: appRouter,
+        return FutureBuilder(
+            future: _userDataResp, 
+            builder: (context, snapshot) {
+                // debugPrint(snapshot.data.toString());
+                if(snapshot.hasData) {
+                    Future(() => ref.read(userDataStateProvider.notifier).setData(snapshot.data));
+                    return MaterialApp.router(routerConfig: appRouter);
+                } else if(snapshot.hasError) {
+                    return MaterialApp.router(routerConfig: appRouter);
+                }
+                
+                return const SplashScreen();
+            },
         );
-
     }
 }
