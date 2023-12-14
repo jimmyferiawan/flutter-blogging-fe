76629a712a293349bbdbcf417b3c1285d0252808
jimmy feriawan <jimyferi@gmail.com>
2023-12-14 14:35:48 +0700

    add : dynamic custom dialog

--- lib/components/custom_dialog.dart
+++ lib/components/custom_dialog.dart
@@ -20,3 +20,24 @@ void showCustomDialog(BuildContext context, String message) {
         }
     );
 }
+
+void showDialogCustomAction({required BuildContext context, String? dialogMessage, String? dialogTitle, String? dialogActionText, void Function(BuildContext)? dialogAction}) {
+    showDialog(
+        context: context, 
+        builder: (BuildContext context) {
+            BuildContext dialogContext = context;
+            return AlertDialog(
+                title: Text(dialogTitle!),
+                content: Text(dialogMessage!),
+                actions: <Widget>[
+                    TextButton(
+                        onPressed: () async{
+                            dialogAction!(dialogContext);
+                        },
+                        child: Text(dialogActionText!),
+                    ),
+                ],
+            );
+        }
+    );
+}
